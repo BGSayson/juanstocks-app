ed42ca08a74a5dd05e0cbe739146b3fe813bd72b
edarasen <edarasen@gmail.com>
2025-09-18 14:19:01 +0800

    empty controllers, devise controllers + views, routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -11,5 +11,20 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  
+  root "welcome#index"
+  
+  devise_for :users, controllers: {
+    registrations: "users/registrations",
+    sessions: "users/sessions"
+  }
+  
+  resources :admins, shallow: true do
+    resources :users
+  end
+  
+  resources :wallets, shallow: true do
+    resources :transactions
+    resources :investments
+  end
 end
