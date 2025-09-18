a8ae58de6bb2f16a494caf2a9d0a6e205368735c
BGSayson <bgsayson@gmail.com>
2025-09-18 18:49:36 +0800

    adds admin and user controller methods, welcome controller, and factory bot gem

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,54 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to admin_users_path # /articles
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+    redirect_to admin_users_path
+    else
+      p @user.errors.messages
+    end
+  end
+  
+  def destroy
+    @user = User.find(params[:id])
+
+    if @user.destroy
+      redirect_to admin_users_path
+    else
+      p @user.errors.messages
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :user_role, :user_status)
+  end
 end
--- spec/requests/admins_spec.rb
+++ spec/requests/admins_spec.rb
@@ -1,7 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe "Admins", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  # Needs to be in every controller test for sign_in method to work
+  include Devise::Test::IntegrationHelpers
+
+  let(:testadmin) { User.new(first_name: "Kelcie", last_name: "Lord", user_role: 1, user_status: 1 ) }
+  
+  setup do
+    @user = :testadmin
+    sign_in @user
   end
+
+  context "get admin index" do
+    it "gets admin index" do
+      get root_path
+      expect(response).to have_http_status(:success)
+    end
+  end
+
 end
