6ef1ec0c5d59e4aa7519a2bec00cee3078ac0d32
edarasen <edarasen@gmail.com>
2025-09-22 15:20:38 +0800

    stock model, database seeding from external api

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema[8.0].define(version: 2025_09_19_121838) do
-=======
 ActiveRecord::Schema[8.0].define(version: 2025_09_20_040845) do
->>>>>>> main
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
