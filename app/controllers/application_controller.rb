6a8135d8c44c642e7d2e8eb90d2c42ee556d1896
edarasen <edarasen@gmail.com>
2025-10-03 21:42:48 +0800

    visual bug fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -35,6 +35,10 @@ class ApplicationController < ActionController::Base
     root_path # your path
   end
 
+  def after_update_path_for(resource)
+    dashboard_path
+  end
+
   # Active Jobs
   OpenexchangerateJob.perform_later
   # FinnhubJob.perform_later
